1f7b2e2115138e262ff94701649a233408156ab5
Irina <dead.rescuer@yandex.com>
2023-10-21 19:49:50 +0300

    Добавляет функцию в function

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,25 @@ const getNumbers = (string) => {
 };
 
 getNumbers('2023');
+
+const getTime = (begin, end, meeting, length) => {
+
+  const [beginHours, beginMinutes] = begin.split(':');
+  begin = (+beginHours * 60) + (+beginMinutes);
+
+  const [endHours, endMinutes] = end.split(':');
+  end = (+endHours * 60) + (+endMinutes);
+
+  const [meetingHours, meetingMinutes] = meeting.split(':');
+  meeting = (+meetingHours * 60) + (+meetingMinutes);
+
+  const num = meeting + length;
+
+  if (begin <= meeting && meeting <= end && num <= end) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(getTime('8:0', '10:0', '8:0', 120));
